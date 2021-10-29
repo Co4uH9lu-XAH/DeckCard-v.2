05219329739aebf8244c0376ef93e53a58c2e231
Co4uH9I-XAH <eugenkunin@gmail.com>
2021-10-29 18:03:52 +0300

    Add Says

---
+++ src/PrisonerSays.java
@@ -0,0 +1,10 @@
+public class PrisonerSays {
+
+    public static void introductorySpeech(){
+        System.out.println("Здорово! Как звать?");
+    }
+    public static void letsPlay (){
+        System.out.println("Давай играть, " + FromCons.saidString+". Сколько карт тянешь?");
+    }
+}
+
