4cddcc1037458945d3a5683f3093a6e2374c861a
Co4uH9I-XAH <eugenkunin@gmail.com>
2021-10-29 17:41:51 +0300

    Add GiveMeCard

--- src/GiveMeCard.java
+++ src/GiveMeCard.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class GiveMeCard {
+
+    public static void main(String[] args) {
+        PrisonerSays.introductorySpeech();
+        FromCons.fromConsString();
+        PrisonerSays.letsPlay();
+        FromCons.fromConsInt();
+
+        DealOfCards.myDeck(FromCons.fromConsInt());
+
+//ArrayList<Card> deck = SaveDeckOfCards.saveDeck();
+        ArrayList<Card> newDeck = new ArrayList<>();
+        newDeck = DealOfCards.myDeck(FromCons.fromConsInt());
+//newDeck.add(RandomCard.getCard(deck));
+        System.out.println(newDeck.toString());
+
+    }
 }
